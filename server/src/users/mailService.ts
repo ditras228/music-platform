7a5955c7195e7a56e82a9599dfb630f5b32fb50b
Dmitry Druzinin <78543687+ditras228@users.noreply.github.com>
2021-08-05 16:44:39 +0300

    smtp fix

--- server/src/users/mailService.ts
+++ server/src/users/mailService.ts
@@ -1,19 +1,19 @@
 const nodemailer = require("nodemailer");
 export async function main(to: string, hash: string) {
     let transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
+        host: "smtp.beget.com",
+        port: 2525,
         secure: false,
         auth: {
-            user: 'druzhinindmitry2020@gmail.com',
-            pass: process.env.EMAILPASSWORD,
+            user: 'druzhinindmitry@druzhinindmitry.ru',
+            pass: process.env.EMAILPASSWORD
         },
     });
 
     let link=`http://87.236.22.121:5002/auth/regconfirm/${hash}`
 
     await transporter.sendMail({
-        from: '"MERNMusic" <druzhinindmitry2020@gmail.com>',
+        from: '"MERNMusic" <druzhinindmitry@druzhinindmitry.ru>',
         to: to,
         subject: "Подтверждение регистрации",
         text: "Спасибо за регистрацию на MERNMusic!",
