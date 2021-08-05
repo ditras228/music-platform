7259f336f94f56cde17060cc2b295bb02658189f
Dmitry Druzinin <78543687+ditras228@users.noreply.github.com>
2021-08-05 22:15:43 +0300

    object to array

--- client/components/create/AlertStep.tsx
+++ client/components/create/AlertStep.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Alert} from '@material-ui/lab'
 import {useFormikContext} from 'formik'
 
 const AlertStep = () => {
     const formik = useFormikContext()  as any
-    const errors = Array.from(formik.errors)
+    let errors = Array.from(formik.errors)
 
 
     for (let i = 0; i < formik.errors.length; i++) {
         errors.push(formik.errors)
     }
+    useEffect(()=>{
+        errors= Array.from(formik.errors)
+    }, formik)
     return (
         <>
             {
